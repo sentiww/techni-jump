f0484bcae6a813bdfe56d47f71ed73053e99b9bd
Jakub Jakubski <36208687+tagisgame@users.noreply.github.com>
2020-06-18 10:21:27 +0200

    build: generate tilemaps from layers
    LevelGenerator can generate new Tilemap for each segment, but Phaser still doesn't allow to display 2 identical layers.

--- src/levelgenerator.ts
+++ src/levelgenerator.ts
@@ -1,40 +1,71 @@
+/**
+ * @classdesc
+ * Level generator class which deals with infinite generation
+ * of map from one Tilemap with multiple layers.
+ *
+ * @class LevelGenerator
+ * @constructor
+ *
+ * @param {Phaser.Tilemaps.Tilemap} mainTilemap created Tilemap object with
+ * layers corresponding to segments.
+ */
 export default class LevelGenerator {
+  /**
+   * Main tilemap object with multiple layers.
+   *
+   * @name LevelGenerator#mainTilemap
+   * @type {Phaser.Tilemaps.Tilemap}
+   */
   private mainTilemap: Phaser.Tilemaps.Tilemap;
-  public segmentsTilemaps: Phaser.Tilemaps.Tilemap[];
+
+  /**
+   * Array of Tilemaps, which will be copied to
+   * generate level sequence.
+   *
+   * @name LevelGenerator#segmentsTemplates
+   * @type {Phaser.Tilemaps.Tilemap[]}
+   */
+  public segmentsTemplates: Phaser.Tilemaps.Tilemap[];
 
   public constructor(mainTilemap: Phaser.Tilemaps.Tilemap) {
     this.mainTilemap = mainTilemap;
 
     /* Converting all layers from original map to different tilemaps */
-    this.segmentsTilemaps =
-        this.mainTilemap.layers.map((el: Phaser.Tilemaps.LayerData) => {
-          let segmentMapConfig: Phaser.Types.Tilemaps.MapDataConfig = {
-            name: el.name,
-            width: el.width,
-            height: el.height,
-            tileWidth: el.tileWidth,
-            tileHeight: el.tileHeight,
-            widthInPixels: el.widthInPixels,
-            heightInPixels: el.heightInPixels,
-            format: this.mainTilemap.format,
-            orientation: this.mainTilemap.orientation,
-            renderOrder: this.mainTilemap.renderOrder,
-            version: this.mainTilemap.version,
-            layers: [el],
-            tilesets: this.mainTilemap.tilesets,
-          }
-
-
-          let mapData: Phaser.Tilemaps.MapData =
-              new Phaser.Tilemaps.MapData(segmentMapConfig);
-          return new Phaser.Tilemaps.Tilemap(this.mainTilemap.scene, mapData);
-        })
-    console.log(this.segmentsTilemaps);
   }
 
-  private randomNumber(min: number, max: number) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+  public generateSegment(segmentId: string): Phaser.Tilemaps.Tilemap{
+    let segmentTemplate =
+        this.mainTilemap.layers.find((layerData: Phaser.Tilemaps.LayerData) => {
+          return layerData.name === segmentId;
+        })
+
+    let segmentMapDataConfig: Phaser.Types.Tilemaps.MapDataConfig = {
+        //name: segmentTemplate.name,
+        width: segmentTemplate.width,
+        height: segmentTemplate.height,
+        tileWidth: segmentTemplate.tileWidth,
+        tileHeight: segmentTemplate.tileHeight,
+        widthInPixels: segmentTemplate.widthInPixels,
+        heightInPixels: segmentTemplate.heightInPixels,
+        format: this.mainTilemap.format,
+        orientation: this.mainTilemap.orientation,
+        renderOrder: this.mainTilemap.renderOrder,
+        version: this.mainTilemap.version,
+        layers: [segmentTemplate],
+        tilesets: this.mainTilemap.tilesets,
+      }
+
+      let segmentMapData: Phaser.Tilemaps.MapData =
+          new Phaser.Tilemaps.MapData(segmentMapDataConfig);
+
+      let segmentTilemap: any =
+          new Phaser.Tilemaps.Tilemap(this.mainTilemap.scene, segmentMapData);
+      segmentTilemap.nextSegments =
+          segmentTemplate.properties.find((property: {name: string}) => {
+            return property.name.toLowerCase() === 'connections';
+          })['value'].split(',');
+      segmentTilemap.currentSegment = segmentTemplate.name;
+
+      return segmentTilemap;
   }
 }
--- src/scenes/sceneLevel.ts
+++ src/scenes/sceneLevel.ts
@@ -6,6 +6,19 @@ export default class Level extends Phaser.Scene
 
   private tileset: any;
 
+  private segmentsList: {
+    displayed: boolean,
+    toDestroy: boolean,
+    tilemap: Phaser.Tilemaps.Tilemap,
+  }[] = [];
+
+  private lastSegmentData: {
+    id: string,
+    nextIds: string[],
+  } = {id: null, nextIds: []};
+
+  private lastDisplayedSegmentX: number = 0;
+
   constructor () {
     super('level');
   }
@@ -22,5 +35,42 @@ export default class Level extends Phaser.Scene
     this.levelGenerator = new LevelGenerator(map);
     this.tileset = map.addTilesetImage(map.tilesets[0].name, 'tileset');
 
+    this.insertSegment('A');
+    for (let i = 0; i < 5; i++) {
+      this.insertSegment(this.randomNextSegmentId(this.lastSegmentData.nextIds));
+    }
+  }
+
+  update() {
+    this.segmentsList.filter(segment => segment.displayed === false)
+        .forEach(segment => {
+          segment.tilemap.createStaticLayer(0, this.tileset, this.lastDisplayedSegmentX, 0);
+          this.lastDisplayedSegmentX += segment.tilemap.widthInPixels;
+          segment.displayed = true;
+          console.log(this.segmentsList, this.lastDisplayedSegmentX)
+        })
+  }
+
+  private insertSegment(segmentId: string): void {
+    let segmentTilemap: any = this.levelGenerator.generateSegment(segmentId);
+
+    this.segmentsList.push({
+      displayed: false,
+      toDestroy: false,
+      tilemap: segmentTilemap,
+    })
+
+    this.lastSegmentData = {
+      id: segmentTilemap.currentSegment,
+      nextIds: segmentTilemap.nextSegments,
+    }
+  }
+
+  private randomNextSegmentId(nextIds: string[]) {
+    let min: number = 0
+    let max: number = nextIds.length;
+    let randomIndex: number = Math.floor(Math.random() * (max - min)) + min;
+
+    return nextIds[randomIndex];
   }
 }
